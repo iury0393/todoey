5d8e1323f0089b1aa44ff0df00e7cb60633fd73d
Iury Vasconcelos <iury0393@gmail.com>
2020-06-03 14:42:36 -0300

    Adicionando o toggle daa check list com callback e state

--- lib/components/tasks_tile.dart
+++ lib/components/tasks_tile.dart
@@ -1,11 +1,43 @@
 import 'package:flutter/material.dart';
 
-class TaskTile extends StatelessWidget {
+class TaskTile extends StatefulWidget {
+  @override
+  _TaskTileState createState() => _TaskTileState();
+}
+
+class _TaskTileState extends State<TaskTile> {
+  bool isChecked = false;
+
   @override
   Widget build(BuildContext context) {
     return ListTile(
-      title: Text('Tarefa 1'),
-      trailing: Checkbox(value: false),
+      title: Text(
+        'Tarefa 1',
+        style: TextStyle(
+            decoration: isChecked ? TextDecoration.lineThrough : null),
+      ),
+      trailing: TaskCheckbox(
+        checkboxState: isChecked,
+        toggleCheckboxState: (bool checkboxState) {
+          setState(() {
+            isChecked = checkboxState;
+          });
+        },
+      ),
+    );
+  }
+}
+
+class TaskCheckbox extends StatelessWidget {
+  TaskCheckbox({this.checkboxState, this.toggleCheckboxState});
+  final bool checkboxState;
+  final Function toggleCheckboxState;
+  @override
+  Widget build(BuildContext context) {
+    return Checkbox(
+      activeColor: Colors.lightBlueAccent,
+      value: checkboxState,
+      onChanged: toggleCheckboxState,
     );
   }
 }
